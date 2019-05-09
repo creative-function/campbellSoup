ec21bcf5591197eff5b2f5d72a7d73c5b8b105a6
Chelsea <chelsea.campbell@my.creativecirucs.edu>
2019-05-09 17:59:09 -0400

    slideshow edits

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,54 @@
 "use strict";
 
-console.log("Hello Chello from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+console.log("Hello Chello! slideshow.");
+
+var slideshow = function slideshow() {
+  console.log('starting slideshow');
+  var currentSlide = -1; //use ALL to grab all elements with that class name
+
+  var $slides = document.querySelectorAll('.slide');
+
+  var newSlide = function newSlide() {
+    //add the next slide
+    currentSlide++;
+    console.log('showing image: ', currentSlide); //if slide reaches the end of its length
+
+    if (currentSlide >= $slides.length) {
+      //start the slide at the first img
+      currentSlide = 0;
+    } //if $slide is currently showing
+
+
+    if ($slides === currentSlide) {
+      //add the showing class, which has display block
+      $slides[currentSlide].className.add("showing");
+    } else {
+      //if it is not showing
+      //remove the showing class, slides set to display hidden 
+      $slides.className.remove("showing");
+    }
+  }; //call the function (load picture)
+
+
+  newSlide(); //every 3 seconcs, call it again (change picture)
+
+  setInterval(newSlide, 3000);
+};
+
+slideshow(); // (function() {
+// 	let currentSlide = -1;
+// 	let $slides = document.querySelectorAll('.slideshow .slide')
+// 	let changeSlide = function() {
+// 		currentSlide++
+// 		if (currentSlide >= $slides.length) {
+// 			currentSlide = 0
+// 		}
+// 		$slides.forEach(function($slide) {
+// 			$slide.style.display = 'none'
+// 		})
+// 		$slides[currentSlide].style.display = 'block'
+// 	}
+// 	changeSlide()
+// 	setInterval(changeSlide, 3000)
+// })();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,71 @@
-console.log(`Hello Chello from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+console.log(`Hello Chello! slideshow.`);
+
+
+
+
+
+let slideshow = function(){
+
+
+    console.log('starting slideshow')
+    let currentSlide=-1;
+
+    //use ALL to grab all elements with that class name
+    let $slides = document.querySelectorAll('.slide');
+
+    let newSlide = function(){
+        
+
+        //add the next slide
+        currentSlide ++
+        console.log('showing image: ', currentSlide);
+    
+        //if slide reaches the end of its length
+        if(currentSlide >= $slides.length){
+            //start the slide at the first img
+            currentSlide = 0
+        }
+
+        //if $slide is currently showing
+        if($slides === currentSlide){
+            //add the showing class, which has display block
+            $slides[currentSlide].className.add("showing");
+        } else { //if it is not showing
+            //remove the showing class, slides set to display hidden 
+            $slides.className.remove("showing");
+        }
+
+
+        
+        
+    }
+    //call the function (load picture)
+    newSlide()
+
+    //every 3 seconcs, call it again (change picture)
+    setInterval(newSlide, 3000)
+};
+
+
+slideshow();
+
+// (function() {
+// 	let currentSlide = -1;
+// 	let $slides = document.querySelectorAll('.slideshow .slide')
+
+
+// 	let changeSlide = function() {
+// 		currentSlide++
+// 		if (currentSlide >= $slides.length) {
+// 			currentSlide = 0
+// 		}
+	 
+// 		$slides.forEach(function($slide) {
+// 			$slide.style.display = 'none'
+// 		})
+// 		$slides[currentSlide].style.display = 'block'
+// 	}
+
+// 	changeSlide()
+// 	setInterval(changeSlide, 3000)
+// })();
